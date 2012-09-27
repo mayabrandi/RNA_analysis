e2d7a2621f3651c4f36344d96f19c128210df3c7
Maya Brandi <maya.brandi@scilifelab.se>
2012-09-27 12:32:18 +0200

    deleted stuff

--- analysis_report.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -l
-cd /bubo/home/h24/mayabr/glob/RNA_analysis
-python /analysis_report.py /bubo/home/h9/mikaelh/Rattus_norvegicus.RGSC3.4.68.gtf /bubo/home/h24/mayabr/config/post_process.yaml /bubo/home/h9/mikaelh/Rat_Baylor_2004_ensembl.bed -c /proj/a2012043/private/nobackup/projects/l_olsson_12_01/intermediate/ -r -s -d -f
